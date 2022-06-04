efc38e8211c2c9c217bc55de5b2b3997314c4dc9
s.dmitriev <s.dmitriev@cian.ru>
2022-06-04 21:36:58 +0300

    fix

---
+++ codegraph.ui/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using codegraph.common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace codegraph.ui.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public IEnumerable<QueueModel> Items { get; set; }  
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet(string name)
+    {
+        var items =  DbWorker.Load();
+        if(string.IsNullOrWhiteSpace(name) ||items==null) return;
+        Items = items.Where(lst => lst.ExchangeName == name || lst.RoutingQueue == name);
+    }
+}
