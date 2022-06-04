42110c0467fdbcaf9b6386c818f556d702e75b51
Олег Сидоров <o.sidorov@cian.ru>
2022-06-04 18:32:44 +0300

    get consumers from solution

--- codegraph/Program.cs
+++ codegraph/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Reflection;
 using System.Reflection.Emit;
 using codegraph;
 using Microsoft.CodeAnalysis;
@@ -20,30 +21,83 @@ catch (Exception e)
 
 void Work()
 {
+    var consumers = new List<MessageSettings>();
+    var publishers = new List<MessageSettings>();
+
     // start Roslyn workspace
     Microsoft.Build.Locator.MSBuildLocator.RegisterDefaults();
     MSBuildWorkspace workspace = MSBuildWorkspace.Create();
 
-// open solution we want to analyze
+    // open solution we want to analyze
     Solution solutionToAnalyze =
         workspace.OpenSolutionAsync(@"C:\Users\o.sidorov\cian\git\microservices\yandex-payments\YandexPayments.sln").Result;
 
-    Project sampleProjectToAnalyze = solutionToAnalyze.Projects
-            .FirstOrDefault(proj => proj.Name == "Scheduler");
-
-// get the project's compilation
-// compilation contains all the types of the 
-// project and the projects referenced by 
-// our project. 
-    Compilation compilation =
-        sampleProjectToAnalyze.GetCompilationAsync().Result;
+    Project project = solutionToAnalyze.Projects
+            .FirstOrDefault(proj => proj.Name == "Common");
     
-    var queueModelType = compilation.GetTypesByMetadataName("Common.Queues.PaymentChangedReportingQueueMessage").FirstOrDefault();
+    Compilation compilation = project.GetCompilationAsync().Result;
+    
+    foreach (var document in project.Documents)
+    {
+        var tree = document.GetSyntaxTreeAsync().Result;
+        var root = tree.GetRoot();
+        var model = compilation.GetSemanticModel(tree);
+        var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
 
-    queueModelType.ToString();
+        foreach (var syntax in classes)
+        {
+            if(IsConsumerQueueModel(syntax))
+            {
+                //var symbol = model.GetDeclaredSymbol(syntax);
+                var constructorDeclarationSyntax = syntax.Members.OfType<ConstructorDeclarationSyntax>().FirstOrDefault();
+                if (constructorDeclarationSyntax != null)
+                {
+                    var arguments = constructorDeclarationSyntax.Initializer.ArgumentList.Arguments;
 
+                    if (arguments.Count == 3)
+                    {
+                        var exchangeName = (model.GetSymbolInfo(arguments[1].Expression).Symbol as IFieldSymbol).ConstantValue.ToString();
+                        var routingKey = arguments[2].Expression.ToString();
+                        
+                        consumers.Add(new MessageSettings()
+                        {
+                            ExchangeName = exchangeName,
+                            RoutingKey = routingKey
+                        });
+                        //Console.WriteLine($"{syntax.Identifier.Value}: {exchangeName} - {routingKey}");
+                    }
+                }
+            }
+            //... need to analyze properties in the class here...
+        }
+    }
+
+    // var queueModelType = compilation.GetTypesByMetadataName("Common.Queues.PaymentChangedReportingQueueMessage").FirstOrDefault();
+    //
+    // queueModelType.ToString();
+    
+    //Assembly.LoadFile()
 }
 
+bool IsConsumerQueueModel(ClassDeclarationSyntax classDeclarationSyntax)
+{
+    return classDeclarationSyntax.BaseList?.Types.FirstOrDefault(x =>
+        "IConsumedQueueMessage" == GetBaseTypeName(x)) != null;
+}
+
+string GetBaseTypeName(BaseTypeSyntax baseTypeSyntax)
+{
+    if (baseTypeSyntax.Type is QualifiedNameSyntax)
+    {
+        return (baseTypeSyntax.Type as QualifiedNameSyntax).Right.ToString();
+    }
+    else if (baseTypeSyntax.Type is IdentifierNameSyntax)
+    {
+        return baseTypeSyntax.Type.ToString();
+    }
+
+    return String.Empty;
+}
 
 // void Work()
 // {
@@ -67,4 +121,11 @@ void Work()
 SyntaxTree GetSyntaxTree(string filepath)
 {
     return CSharpSyntaxTree.ParseText(File.ReadAllText(filepath));
+}
+
+
+public class MessageSettings
+{
+    public string ExchangeName { get; set; }
+    public string RoutingKey { get; set; }
 }
