75e9c371a82ea84a3980f612bf918841a8733cfc
Evgeniy Peshkov <e.peshkov@gmail.com>
2022-06-04 23:45:11 +0300

    render graph

---
+++ codegraph.ui/Pages/All.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace codegraph.ui.Pages
+{
+    public class AllModel : PageModel
+    {
+        public void OnGet()
+        {
+            var items =  DbWorker.Load()??new List<QueueModel>();
+           
+            var consumers = items.Where(lst =>  lst.MemberType ==  QueueMemberType.Consumer).ToArray();
+            var publishers = items.Where(lst =>  lst.MemberType ==  QueueMemberType.Publisher);
+
+            Edges = new List<Tuple<string, string>>();
+
+            foreach (var publisher in publishers)
+            {
+                var linkedConsumers = consumers.Where(x => x.ExchangeName == publisher.ExchangeName && x.RoutingKey == publisher.RoutingKey);
+                foreach (var consumer in linkedConsumers)
+                {
+                    edges.Add(new Tuple<string, string>(publisher.Microservice, consumer.Microservice));
+                }
+            }
+            
+            
+        }
+
+        public List<Tuple<string, string>> Edges { get; set; }
+        
+    }
+}
